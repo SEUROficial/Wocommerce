0ffe42658e8a2d71eaf9aaf4d03a4b384a372d2b
José Conti <j.conti@joseconti.com>
2017-03-17 12:47:20 +0100

    uninstaller updated

--- uninstall.php
+++ uninstall.php
@@ -1,35 +1,56 @@
 <?php
 
 //if uninstall not called from WordPress exit
-if (!defined('WP_UNINSTALL_PLUGIN') || !current_user_can('activate_plugins')) exit();
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) || ! current_user_can( 'activate_plugins' ) ) exit();
 
 global $wpdb;
-/*require_once dirname(__FILE__).'/config.php';
 
-Desinstalar();
+// remove options added by SEUR Plugin
 
-function Desinstalar(){
-		global $wpdb;
+$options = array(
+    'seur_after_get_label_field',
+    'seur_preaviso_notificar_field',
+    'seur_reparto_notificar_field',
+    'seur_tipo_notificacion_field',
+    'seur_manana_desde_field',
+    'seur_manana_hasta_field',
+    'seur_tarde_desde_field',
+    'seur_tarde_hasta_field',
+    'seur_tipo_etiqueta_field',
+    'seur_aduana_origen_field',
+    'seur_aduana_destino_field',
+    'seur_tipo_mercancia_field',
+    'seur_valor_declarado_field',
+    'seur_id_mercancia_field',
+    'seur_descripcion_field',
+    'seur_nif_field',
+    'seur_empresa_field',
+    'seur_viatipo_field',
+    'seur_vianombre_field',
+    'seur_vianumero_field',
+    'seur_escalera_field',
+    'seur_piso_field',
+    'seur_puerta_field',
+    'seur_postal_field',
+    'seur_poblacion_field',
+    'seur_provincia_field',
+    'seur_pais_field',
+    'seur_telefono_field',
+    'seur_email_field',
+    'seur_contacto_nombre_field',
+    'seur_contacto_apellidos_field',
+    'seur_cit_codigo_field',
+    'seur_cit_usuario_field',
+    'seur_cit_contra_field',
+    'seur_ccc_field',
+    'seur_franquicia_field',
+    'seur_seurcom_usuario_field',
+    'seur_seurcom_contra_field'
+    );
 
-		// Eliminamos toda la tabla de configuracion de la tienda
-		$tabla = $wpdb->prefix . SEUR_PLUGIN_TABLA;
-		$wpdb->query("DROP TABLE IF EXISTS $tabla");
+foreach ( $options as $option ){
 
-		// eliminamos la tabla de las recogidas
-		$tabla = $wpdb->prefix . SEUR_PLUGIN_RECO;
-		$wpdb->query("DROP TABLE IF EXISTS $tabla");
+    delete_option( $option );
 
-		// eliminamos la tabla de las etiquetas impresas
-		$tabla = $wpdb->prefix . SEUR_PLUGIN_ECB;
-		$wpdb->query("DROP TABLE IF EXISTS $tabla");
-
-			// eliminamos la tabla de servicios productos
-		$tabla = $wpdb->prefix . SEUR_PLUGIN_SVPR;
-		$wpdb->query("DROP TABLE IF EXISTS $tabla");
-
-		$tabla = $wpdb->prefix . SEUR_PLUGIN_SVPR;
-		// Si existe la carpeta de descargas la borramos
-		if (is_dir(SEUR_DESCARGAS)) rmdir(SEUR_DESCARGAS);
-*/
-	}
+}
 
