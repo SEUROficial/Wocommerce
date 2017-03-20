91d5c7f866f751e1cd6e4a6244925ffb4f2065d7
José Conti <j.conti@joseconti.com>
2017-03-20 21:43:36 +0100

    Added button and the use of seur_after_get_label() function

--- core/pages/seur-get-labels.php
+++ core/pages/seur-get-labels.php
@@ -1,17 +1,19 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-	function seur_get_labels_from_order( $post ){
+	function seur_get_labels_from_order( $post ){?>
+
+    <?php
 
 		if ( ! current_user_can('level_10') )
 		die( __( 'Cheatin&#8217; uh?', SEUR_TEXTDOMAIN ) );
-		$orderID = '';
-		$orderID = absint( $_GET["order_id"] );
+		$orderID   = '';
+		$orderID   = absint( $_GET["order_id"] );
 		$order_id  = '';
 		$order_id  = $_POST['order-id'];
-		$weight = '';
+		$weight    = '';
 		$weight    = get_post_meta( $orderID, '_seur_cart_weight', true );
-		$packages = '';
-
+		$packages  = '';
+		if( ! $orderID && ! $order_id ) exit;
 		?>
 		<div class="wrap">
 		<h1 class="wp-heading-inline"><?php _e( 'Get Labels', SEUR_TEXTDOMAIN ); ?></h1>
@@ -28,6 +30,8 @@
 	            <?php wp_nonce_field( 'seur_get_label_action', 'seur_get_label_nonce_field' ); ?>
 	            <input type="submit" class="seur_label_submit button button-primary" value="<?php _e( 'Get labels', SEUR_TEXTDOMAIN ); ?>" />
 			</form>
+			<br />
+			<a class="button" href="#" onclick="self.parent.tb_remove(); self.parent.location.reload()"><?php _e('Close', SEUR_TEXTDOMAIN); ?></a>
 
 		<?php	} elseif ( $order_id ) {
 
@@ -37,15 +41,21 @@
 					$has_label   = '';
 					$label_id    = '';
 
-				    if ( ! isset($_POST['seur-weight'] ) || ! isset($_POST['seur-number-packages'] ) ) {
-					    return $post_id;
+				    if ( ! $weight ) {
+					    $message = __( 'Weight is needed', SEUR_TEXTDOMAIN );
+					    die( $message );
+					    }
+
+					if ( ! $numpackages ) {
+					    $message = __( 'Package number is needed', SEUR_TEXTDOMAIN );
+					    die( $message );
 					    }
 
 				    if ( ! isset( $_POST['seur_get_label_nonce_field'] ) || ! wp_verify_nonce( $_POST['seur_get_label_nonce_field'], 'seur_get_label_action' ) ) {
-					    return $post_id;
+					    exit;
 					    }
 
-				    $new_status  = 'seur-shipment';
+				    $new_status  = seur_after_get_label();
 				    $has_label   = get_post_meta( $order_id, '_seur_shipping_order_label_downloaded', true );
 
 				    if ( $has_label != 'yes' ) {
@@ -59,12 +69,16 @@
 				            add_post_meta( $order_id,'_seur_shipping_order_label_downloaded',  'yes', true );
 				            add_post_meta( $order_id,'_seur_shipping_label_id',  $label_id, true );
 				            $order->add_order_note( 'The Label for Order #' . $post_id . ' have been downloaded', 0, true);
-				            echo 'Label goted, the ID is ' . $label_id;
-
+				            echo __('Label dowloaded, the Label ID is ', SEUR_TEXTDOMAIN ) . $label_id; ?>
+				            <br />
+							<a class="button" href="#" onclick="self.parent.tb_remove(); self.parent.location.reload()"><?php _e('Close', SEUR_TEXTDOMAIN); ?></a>
+							<?php
 				        }
 				    } else {
-					    _e('The order has a label jet', SEUR_TEXTDOMAIN );
-				    }
+					    _e('The Order already has a label', SEUR_TEXTDOMAIN ); ?>
+					    <br />
+						<a class="button" href="#" onclick="self.parent.tb_remove(); self.parent.location.reload()"><?php _e('Close', SEUR_TEXTDOMAIN); ?></a>
+				   <?php }
     	} ?>
 		</div>
 <?php	}
