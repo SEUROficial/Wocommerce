f34db54c7e759e44c55f5789322db8cab53e0f1b
José Conti <j.conti@joseconti.com>
2017-03-21 01:48:33 +0100

    tagging v1.0.0-Beta2

--- loader.php
+++ loader.php
@@ -3,7 +3,7 @@
 Plugin Name: SEUR Official
 Plugin URI: http://www.seur.com/
 Description: SEUR Official
-Version: 1.0.0-Beta1
+Version: 1.0.0-Beta2
 Author: José Conti
 Author URI: https://www.joseconti.com/
 Tested up to: 4.7
@@ -13,7 +13,7 @@ License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
 
-define( 'SEUR_OFFICIAL_VERSION',    '1.0.0-Beta1'               );
+define( 'SEUR_OFFICIAL_VERSION',    '1.0.0-Beta2'               );
 define( 'SEUR_DB_VERSION',          '1.0.0'                     );
 define( 'SEUR_TABLE_VERSION',       '1.0.0'                     );
 define( 'SEUR_PLUGIN_PATH',         plugin_dir_path( __FILE__ ) );
