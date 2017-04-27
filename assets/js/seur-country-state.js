fca51822e4bb79086f06600c4a41b2b989d81cf2
José Conti <j.conti@joseconti.com>
2017-04-28 01:15:29 +0200

    Added JS for rates

--- assets/js/seur-country-state.js
+++ assets/js/seur-country-state.js
@@ -1,6 +1,17 @@
 jQuery(document).ready(function($){
     var seur_country_state_process = 'admin.php?page=seur_country_state_process';
 
+     $("select.rate").change(function(){
+        var selectedRate = $(".rate option:selected").val();
+        $.ajax({
+            type: "POST",
+            url: seur_country_state_process,
+            data: { rate : selectedRate }
+        }).done(function(data){
+            $("#countryid").replaceWith( $("#countryid").html(data) );
+        });
+    });
+
     $("select.country").change(function(){
         var selectedCountry = $(".country option:selected").val();
         $.ajax({
