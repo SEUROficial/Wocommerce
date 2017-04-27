06beb81d07ca7bb155319e06c6fe90e2f3de188a
José Conti <j.conti@joseconti.com>
2017-04-28 01:17:07 +0200

    Auto load country/rate

--- core/pages/rates/seur-country-state-process.php
+++ core/pages/rates/seur-country-state-process.php
@@ -1,17 +1,64 @@
 <?php
 function seur_country_state_process() {
 
-	if(isset($_POST["country"])){
+	if( isset( $_POST[ "rate" ] ) ) {
+
+		$rate = $_POST[ "rate" ];
+
+		if ( $rate == 'PARTICULARES 24H ESTANDAR' ){
+			echo '<select class="select country" id="country" title="' . __('Select Country', SEUR_TEXTDOMAIN ) . '" name="country">';
+			echo '<option value="Select">' . __('Select a Country', SEUR_TEXTDOMAIN ) . '</option>';
+			echo '<option value="ES">Spain</option>';
+			echo '</select>';
+
+			} else { ?>
+				<select class="select country" id="country" title="<?php _e('Select Country', SEUR_TEXTDOMAIN ); ?>" name="country">
+				    <?php
+						echo '<option value="Select">' . __('Select a Country', SEUR_TEXTDOMAIN ) . '</option>';
+						echo '<option value="*">' . __( 'All Countries', SEUR_TEXTDOMAIN ) . '</option>';
+	                    $countries = seur_get_countries();
+						foreach ($countries as $countrie => $value )
+							{
+								echo '<option value="' . $countrie  . '">' . $value . '</option>';
+							}
+						echo '<option value="REST">' . __( 'Rest of the World', SEUR_TEXTDOMAIN ) . '</option>';
+					?>
+				</select>
+				<?php
+
+			}
+
+		}
+
+
+	/*
+
+	<select class="select country" id="country" title="<?php _e('Select Country', SEUR_TEXTDOMAIN ); ?>" name="country">
+				    <?php
+						echo '<option value="Select">' . __('Select a Country', SEUR_TEXTDOMAIN ) . '</option>';
+						echo '<option value="*">' . __( 'All Countries', SEUR_TEXTDOMAIN ) . '</option>';
+	                    $countries = seur_get_countries();
+						foreach ($countries as $countrie => $value )
+							{
+								echo '<option value="' . $countrie  . '">' . $value . '</option>';
+							}
+						echo '<option value="REST">' . __( 'Rest of the World', SEUR_TEXTDOMAIN ) . '</option>';
+					?>
+				</select>
+
+	*/
+
+	if( isset( $_POST[ "country" ] ) ) {
 	    // Capture selected country
-	    $country = $_POST["country"];
+	    $country = $_POST[ "country" ];
 
 	    // Define country and city array
 	    $countryArr = seur_get_countries_states( $country );
 
 	    if( $countryArr ){
 	    // Display city dropdown based on country name
-		    if($country !== 'Select'){
-		        echo '<select title="' . __('Select State', SEUR_TEXTDOMAIN ) . '" name="state">';
+		    if( $country !== 'Select' ) {
+		        echo '<select title="' . __( 'Select State', SEUR_TEXTDOMAIN ) . '" name="state">';
 		        echo '<option value="*">' . __('All States', SEUR_TEXTDOMAIN ) . '</option>';
 		        foreach( $countryArr as $state => $value ){
 
