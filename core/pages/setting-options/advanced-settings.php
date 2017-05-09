ca0294c35751406c8107fc49ba84cedd8a6196e9
José Conti <j.conti@joseconti.com>
2017-05-10 00:44:23 +0200

    Updated advanced settings page

--- core/pages/setting-options/advanced-settings.php
+++ core/pages/setting-options/advanced-settings.php
@@ -4,7 +4,6 @@ function seur_after_get_label_field(){
    $option = get_option( 'seur_after_get_label_field' );
    ?>
    <select id="notification_type" name="seur_after_get_label_field">
-       <option value="default"> <?php _e( "Select what to do", SEUR_TEXTDOMAIN ); ?> </option>
        <option value="shipping" <?php if ( $option == 'shipping') echo ' selected'; ?>><?php _e('Mark as Shipping', SEUR_TEXTDOMAIN ); ?></option>
        <option value="complete" <?php if ( $option == 'complete') echo ' selected'; ?>><?php _e('Mark as Complete', SEUR_TEXTDOMAIN ); ?></option>
     </select>
@@ -23,52 +22,93 @@ function seur_tipo_notificacion_field(){
    $option = get_option( 'seur_tipo_notificacion_field' );
    ?>
    <select id="notification_type" name="seur_tipo_notificacion_field">
-       <option value="default"> <?php _e( "Select notification Type", SEUR_TEXTDOMAIN ); ?> </option>
        <option value="SMS" <?php if ( $option == 'SMS') echo ' selected'; ?>><?php _e('SMS (this option has an extra cost)', SEUR_TEXTDOMAIN); ?></option>
        <option value="EMAIL" <?php if ( $option == 'EMAIL') echo ' selected'; ?>>Email</option>
     </select>
 <?php   }
 
-function seur_manana_desde_field(){ ?>
-    <input title="<?php _e('SEUR field description', SEUR_TEXTDOMAIN ); ?>" type="text" name="seur_manana_desde_field" value="<?php echo get_option('seur_manana_desde_field'); ?>" size="40" />
+function seur_manana_desde_field(){
+    $option = get_option( 'seur_manana_desde_field' );
+   ?>
+   <select id="manana_desde_type" name="seur_manana_desde_field">
+       <option value="09:00" <?php if ( $option == '09:00') echo ' selected'; ?>>09:00</option>
+       <option value="10:00" <?php if ( $option == '10:00') echo ' selected'; ?>>10:00</option>
+       <option value="11:00" <?php if ( $option == '11:00') echo ' selected'; ?>>11:00</option>
+       <option value="12:00" <?php if ( $option == '12:00') echo ' selected'; ?>>12:00</option>
+    </select>
     <?php }
 
-function seur_manana_hasta_field(){ ?>
-    <input title="<?php _e('SEUR field description', SEUR_TEXTDOMAIN ); ?>" type="text" name="seur_manana_hasta_field" value="<?php echo get_option('seur_manana_hasta_field'); ?>" size="40" />
+function seur_manana_hasta_field(){
+    $option = get_option( 'seur_manana_hasta_field' );
+   ?>
+   <select id="manana_hasta_type" name="seur_manana_hasta_field">
+       <option value="11:00" <?php if ( $option == '11:00') echo ' selected'; ?>>11:00</option>
+       <option value="12:00" <?php if ( $option == '12:00') echo ' selected'; ?>>12:00</option>
+       <option value="13:00" <?php if ( $option == '13:00') echo ' selected'; ?>>13:00</option>
+       <option value="14:00" <?php if ( $option == '14:00') echo ' selected'; ?>>14:00</option>
+    </select>
     <?php }
 
-function seur_tarde_desde_field(){ ?>
-    <input title="<?php _e('SEUR field description', SEUR_TEXTDOMAIN ); ?>" type="text" name="seur_tarde_desde_field" value="<?php echo get_option('seur_tarde_desde_field'); ?>" size="40" />
+function seur_tarde_desde_field(){
+    $option = get_option( 'seur_tarde_desde_field' );
+   ?>
+   <select id="tarde_desde_type" name="seur_tarde_desde_field">
+       <option value="15:00" <?php if ( $option == '15:00') echo ' selected'; ?>>15:00</option>
+       <option value="16:00" <?php if ( $option == '16:00') echo ' selected'; ?>>16:00</option>
+       <option value="17:00" <?php if ( $option == '17:00') echo ' selected'; ?>>17:00</option>
+       <option value="18:00" <?php if ( $option == '18:00') echo ' selected'; ?>>18:00</option>
+    </select>
     <?php }
 
-function seur_tarde_hasta_field(){ ?>
-    <input title="<?php _e('SEUR field description', SEUR_TEXTDOMAIN ); ?>" type="text" name="seur_tarde_hasta_field" value="<?php echo get_option('seur_tarde_hasta_field'); ?>" size="40" />
+function seur_tarde_hasta_field(){
+    $option = get_option( 'seur_tarde_hasta_field' );
+   ?>
+   <select id="tarde_hasta_type" name="seur_tarde_hasta_field">
+       <option value="17:00" <?php if ( $option == '17:00') echo ' selected'; ?>>17:00</option>
+       <option value="18:00" <?php if ( $option == '18:00') echo ' selected'; ?>>18:00</option>
+       <option value="19:00" <?php if ( $option == '19:00') echo ' selected'; ?>>19:00</option>
+       <option value="20:00" <?php if ( $option == '20:00') echo ' selected'; ?>>20:00</option>
+    </select>
     <?php }
 
 function seur_tipo_etiqueta_field(){
    $option = get_option( 'seur_tipo_etiqueta_field' );
    ?>
    <select id="label_type" name="seur_tipo_etiqueta_field">
-       <option value="default"> <?php _e( "Select Label Type", SEUR_TEXTDOMAIN ); ?> </option>
        <option value="PDF" <?php if ( $option == 'PDF') echo ' selected'; ?>>PDF</option>
        <option value="TERMICA" <?php if ( $option == 'TERMICA') echo ' selected'; ?>>TERMICA</option>
     </select>
 <?php   }
 
-function seur_aduana_origen_field(){ ?>
-    <input title="<?php _e('SEUR field description', SEUR_TEXTDOMAIN ); ?>" type="text" name="seur_aduana_origen_field" value="<?php echo get_option('seur_aduana_origen_field'); ?>" size="40" />
+function seur_aduana_origen_field(){
+    $option = get_option( 'seur_aduana_origen_field' );
+   ?>
+   <select id="seur_aduana_origen_type" name="seur_aduana_origen_field">
+       <option value="D" <?php if ( $option == 'D') echo ' selected'; ?>>D</option>
+       <option value="F" <?php if ( $option == 'F') echo ' selected'; ?>>F</option>
+
+    </select>
     <?php }
 
-function seur_aduana_destino_field(){ ?>
-    <input title="<?php _e('SEUR field description', SEUR_TEXTDOMAIN ); ?>" type="text" name="seur_aduana_destino_field" value="<?php echo get_option('seur_aduana_destino_field'); ?>" size="40" />
+function seur_aduana_destino_field(){
+    $option = get_option( 'seur_aduana_destino_field' );
+   ?>
+   <select id="seur_aduana_destino_type" name="seur_aduana_destino_field">
+       <option value="D" <?php if ( $option == 'D') echo ' selected'; ?>>D</option>
+       <option value="F" <?php if ( $option == 'F') echo ' selected'; ?>>F</option>
+
+    </select>
     <?php }
 
-function seur_tipo_mercancia_field(){ ?>
-    <input title="<?php _e('SEUR field description', SEUR_TEXTDOMAIN ); ?>" type="text" name="seur_tipo_mercancia_field" value="<?php echo get_option('seur_tipo_mercancia_field'); ?>" size="40" />
-    <?php }
-
-function seur_valor_declarado_field(){ ?>
-    <input title="<?php _e('SEUR field description', SEUR_TEXTDOMAIN ); ?>" type="text" name="seur_valor_declarado_field" value="<?php echo get_option('seur_valor_declarado_field'); ?>" size="40" />
+function seur_tipo_mercancia_field(){
+    $option = get_option( 'seur_tipo_mercancia_field' );
+   ?>
+   <select id="mercancia_type" name="seur_tipo_mercancia_field">
+       <option value="C" <?php if ( $option == 'C') echo ' selected'; ?>><?php _e( 'C: Commercial', SEUR_TEXTDOMAIN ); ?></option>
+       <option value="D" <?php if ( $option == 'D') echo ' selected'; ?>><?php _e( 'D: Documents', SEUR_TEXTDOMAIN ); ?></option>
+       <option value="N" <?php if ( $option == 'N') echo ' selected'; ?>><?php _e( 'N: No Commercial', SEUR_TEXTDOMAIN ); ?></option>
+       <option value="S" <?php if ( $option == 'S') echo ' selected'; ?>><?php _e( 'S: Envelopes', SEUR_TEXTDOMAIN ); ?></option>
+    </select>
     <?php }
 
 function seur_id_mercancia_field(){ ?>
@@ -95,7 +135,6 @@ function display_seur_advanced_settings_panel_fields(){
     add_settings_field( 'seur_aduana_origen_field',         __('Aduana origen',             SEUR_TEXTDOMAIN), 'seur_aduana_origen_field',           'seur-advanced-settings-options', 'seur-advanced-settings-section' );
     add_settings_field( 'seur_aduana_destino_field',        __('Aduana destino',            SEUR_TEXTDOMAIN), 'seur_aduana_destino_field',          'seur-advanced-settings-options', 'seur-advanced-settings-section' );
     add_settings_field( 'seur_tipo_mercancia_field',        __('Tipo mercancia',            SEUR_TEXTDOMAIN), 'seur_tipo_mercancia_field',          'seur-advanced-settings-options', 'seur-advanced-settings-section' );
-    add_settings_field( 'seur_valor_declarado_field',       __('Valor declarado',           SEUR_TEXTDOMAIN), 'seur_valor_declarado_field',         'seur-advanced-settings-options', 'seur-advanced-settings-section' );
     add_settings_field( 'seur_id_mercancia_field',          __('ID Mercancia',              SEUR_TEXTDOMAIN), 'seur_id_mercancia_field',            'seur-advanced-settings-options', 'seur-advanced-settings-section' );
     add_settings_field( 'seur_descripcion_field',           __('Descripción internacional', SEUR_TEXTDOMAIN), 'seur_descripcion_field',             'seur-advanced-settings-options', 'seur-advanced-settings-section' );
 
@@ -113,7 +152,6 @@ function display_seur_advanced_settings_panel_fields(){
     register_setting('seur-advanced-settings-section', 'seur_aduana_origen_field'           );
     register_setting('seur-advanced-settings-section', 'seur_aduana_destino_field'          );
     register_setting('seur-advanced-settings-section', 'seur_tipo_mercancia_field'          );
-    register_setting('seur-advanced-settings-section', 'seur_valor_declarado_field'         );
     register_setting('seur-advanced-settings-section', 'seur_id_mercancia_field'            );
     register_setting('seur-advanced-settings-section', 'seur_descripcion_field'             );
 
