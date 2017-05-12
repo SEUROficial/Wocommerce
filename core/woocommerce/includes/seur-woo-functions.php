8f5415da9324c3d1f2917cec44b69c778a64b066
José Conti <j.conti@joseconti.com>
2017-05-12 17:15:16 +0200

    Renaming label & removing test function

--- core/woocommerce/includes/seur-woo-functions.php
+++ core/woocommerce/includes/seur-woo-functions.php
@@ -84,7 +84,7 @@ add_filter( 'wc_order_statuses', 'seur_add_awaiting_shipment_status' );
 function seur_register_awaiting_labels_status() {
 
     register_post_status( 'wc-seur-label', array(
-        'label'     => 'Awaiting Labels',
+        'label'     => 'Awaiting SEUR Labels',
         'public'    => true,
         'show_in_admin_status_list' => true, // show count All (12) , Completed (9) , Awaiting shipment (2) ...
         'label_count'   => _n_noop( __('Awaiting SEUR Label <span class="count">(%s)</span>', SEUR_TEXTDOMAIN ), __('Awaiting SEUR Label <span class="count">(%s)</span>', SEUR_TEXTDOMAIN ) )
@@ -106,7 +106,7 @@ function seur_add_awaiting_labels_status( $wc_statuses_arr ) {
         $new_statuses_arr[ $id ] = $label;
 
         if ( 'wc-processing' === $id ) { // after "Completed" status
-            $new_statuses_arr['wc-seur-label'] = 'Awaiting Label';
+            $new_statuses_arr['wc-seur-label'] = 'Awaiting SEUR Label';
         }
     }
 
@@ -360,101 +360,4 @@ add_action( 'woocommerce_admin_order_data_after_shipping_address', 'seur_shippin
 
 function seur_shipping_mobil_phone_fields_display_admin_order_meta( $order ){
     echo '<p><strong>'.__('Shipping Mobile Phone').':</strong> ' . get_post_meta( $order->id, '_shipping_mobile_phone', true ) . '</p>';
-}
-
-
-/******* TEMP FUNCTION **************/
-
-function get_order_details( $order_id ){
-
-    // 1) Get the Order object
-    $order = wc_get_order( $order_id );
-
-    // OUTPUT
-    echo '<h3>RAW OUTPUT OF THE ORDER OBJECT: </h3>';
-    print_r($order);
-    echo '<br><br>';
-    echo '<h3>THE ORDER OBJECT (Using the object syntax notation):</h3>';
-    echo '$order->order_type: ' . $order->order_type . '<br>';
-    echo '$order->id: ' . $order->id . '<br>';
-    echo '<h4>THE POST OBJECT:</h4>';
-    echo '$order->post->ID: ' . $order->post->ID . '<br>';
-    echo '$order->post->post_author: ' . $order->post->post_author . '<br>';
-    echo '$order->post->post_date: ' . $order->post->post_date . '<br>';
-    echo '$order->post->post_date_gmt: ' . $order->post->post_date_gmt . '<br>';
-    echo '$order->post->post_content: ' . $order->post->post_content . '<br>';
-    echo '$order->post->post_title: ' . $order->post->post_title . '<br>';
-    echo '$order->post->post_excerpt: ' . $order->post->post_excerpt . '<br>';
-    echo '$order->post->post_status: ' . $order->post->post_status . '<br>';
-    echo '$order->post->comment_status: ' . $order->post->comment_status . '<br>';
-    echo '$order->post->ping_status: ' . $order->post->ping_status . '<br>';
-    echo '$order->post->post_password: ' . $order->post->post_password . '<br>';
-    echo '$order->post->post_name: ' . $order->post->post_name . '<br>';
-    echo '$order->post->to_ping: ' . $order->post->to_ping . '<br>';
-    echo '$order->post->pinged: ' . $order->post->pinged . '<br>';
-    echo '$order->post->post_modified: ' . $order->post->post_modified . '<br>';
-    echo '$order->post->post_modified_gtm: ' . $order->post->post_modified_gtm . '<br>';
-    echo '$order->post->post_content_filtered: ' . $order->post->post_content_filtered . '<br>';
-    echo '$order->post->post_parent: ' . $order->post->post_parent . '<br>';
-    echo '$order->post->guid: ' . $order->post->guid . '<br>';
-    echo '$order->post->menu_order: ' . $order->post->menu_order . '<br>';
-    echo '$order->post->post_type: ' . $order->post->post_type . '<br>';
-    echo '$order->post->post_mime_type: ' . $order->post->post_mime_type . '<br>';
-    echo '$order->post->comment_count: ' . $order->post->comment_count . '<br>';
-    echo '$order->post->filter: ' . $order->post->filter . '<br>';
-    echo '<h4>THE ORDER OBJECT (again):</h4>';
-    echo '$order->order_date: ' . $order->order_date . '<br>';
-    echo '$order->modified_date: ' . $order->modified_date . '<br>';
-    echo '$order->customer_message: ' . $order->customer_message . '<br>';
-    echo '$order->customer_note: ' . $order->customer_note . '<br>';
-    echo '$order->post_status: ' . $order->post_status . '<br>';
-    echo '$order->prices_include_tax: ' . $order->prices_include_tax . '<br>';
-    echo '$order->tax_display_cart: ' . $order->tax_display_cart . '<br>';
-    echo '$order->display_totals_ex_tax: ' . $order->display_totals_ex_tax . '<br>';
-    echo '$order->display_cart_ex_tax: ' . $order->display_cart_ex_tax . '<br>';
-    echo '$order->formatted_billing_address->protected: ' . $order->formatted_billing_address->protected . '<br>';
-    echo '$order->formatted_shipping_address->protected: ' . $order->formatted_shipping_address->protected . '<br><br>';
-    echo '- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <br><br>';
-
-    // 2) Get the Order meta data
-    $order_meta = get_post_meta($order_id);
-
-    echo '<h3>RAW OUTPUT OF THE ORDER META DATA (ARRAY): </h3>';
-    //echo '<pre>';
-    print_r($order_meta);
-    //echo '</pre>';
-    echo '<br><br>';
-    echo '<h3>THE ORDER META DATA (Using the array syntax notation):</h3>';
-    echo '$order_meta[_order_key][0]: ' . $order_meta['_order_key'][0] . '<br>';
-    echo '$order_meta[_order_currency][0]: ' . $order_meta['_order_currency'][0] . '<br>';
-    echo '$order_meta[_prices_include_tax][0]: ' . $order_meta['_prices_include_tax'][0] . '<br>';
-    echo '$order_meta[_customer_user][0]: ' . $order_meta['_customer_user'][0] . '<br>';
-    echo '$order_meta[_billing_first_name][0]: ' . $order_meta['_billing_first_name'][0] . '<br><br>';
-    echo 'And so on ……… <br><br>';
-    echo '- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <br><br>';
-
-    // 3) Get the order items
-    $items = $order->get_items();
-
-    echo '<h3>RAW OUTPUT OF THE ORDER ITEMS DATA (ARRAY): </h3>';
-
-    foreach ( $items as $item_id => $item_data ) {
-
-        echo '<h4>RAW OUTPUT OF THE ORDER ITEM NUMBER: '. $item_id .'): </h4>';
-        echo '<pre>';
-        print_r($item_data);
-        echo '</pre>';
-        echo '<br><br>';
-        echo 'Item ID: ' . $item_id. '<br>';
-        echo '$item["product_id"] <i>(product ID)</i>: ' . $item_data['product_id'] . '<br>';
-         echo 'producto url: <a href="' . get_home_url() . '/?p=' . $item_data['product_id'] . '" target="_blank">' . $item_data['name'] . '</a><br>';
-        echo '$item["name"] <i>(product Name)</i>: ' . $item_data['name'] . '<br>';
-
-        // Using get_item_meta() method
-        echo 'Item quantity <i>(product quantity)</i>: ' . $order->get_item_meta($item_id, '_qty', true) . '<br><br>';
-        echo 'Item line total <i>(product quantity)</i>: ' . $order->get_item_meta($item_id, '_line_total', true) . '<br><br>';
-        echo 'And so on ……… <br><br>';
-        echo '- - - - - - - - - - - - - <br><br>';
-    }
-    echo '- - - - - - E N D - - - - - <br><br>';
 }
