a49220763e44c4de033cb15e0a04c4baf13fd39f
José Conti <j.conti@joseconti.com>
2017-05-17 01:06:17 +0200

    Added logic to country /State

--- core/pages/rates/seur-country-state-process.php
+++ core/pages/rates/seur-country-state-process.php
@@ -1,7 +1,7 @@
 <?php
 function seur_country_state_process() {
 
-	if( isset( $_POST[ "rate" ] ) ) {
+	if ( isset( $_POST[ "rate" ] ) ) {
 
 		$rate = $_POST[ "rate" ];
 
@@ -12,9 +12,17 @@ function seur_country_state_process() {
 			echo '<option value="ES">' . __('Spain', SEUR_TEXTDOMAIN ) . '</option>';
 			echo '</select>';
 
+			} elseif ( $rate == 'SEUR 72H Estándar' || $rate == 'SEUR 48H Estándar' ) {
+
+				echo '<select class="select country" id="country" title="' . __('Select Country', SEUR_TEXTDOMAIN ) . '" name="country">';
+				echo '<option value="NULL">' . __('Select Country', SEUR_TEXTDOMAIN ) . '</option>';
+				echo '<option value="ES">' . __('Spain', SEUR_TEXTDOMAIN ) . '</option>';
+				echo '</select>';
+
 			} else { ?>
 				<select class="select country" id="country" title="<?php _e('Select Country', SEUR_TEXTDOMAIN ); ?>" name="country">
 				    <?php
+						echo '<option value="NULL">' . __( 'Select', SEUR_TEXTDOMAIN ) . '</option>';
 						echo '<option value="*">' . __( 'All Countries', SEUR_TEXTDOMAIN ) . '</option>';
 	                    $countries = seur_get_countries();
 						foreach ($countries as $countrie => $value )
@@ -26,28 +34,49 @@ function seur_country_state_process() {
 				<?php
 
 			}
+			set_transient( get_current_user_id() . '_seur_rate', $rate );
 
 		}
 
-	if( isset( $_POST[ "country" ] ) ) {
+	if ( isset( $_POST[ "country" ] ) ) {
 	    // Capture selected country
 	    $country = $_POST[ "country" ];
+	    $rate = get_transient( get_current_user_id() . '_seur_rate' );
 
 	    // Define country and city array
 	    $countryArr = seur_get_countries_states( $country );
 
-	    if( $countryArr ){
-	    // Display city dropdown based on country name
-		    if( $country !== 'Select' ) {
-		        echo '<select title="' . __( 'Select State', SEUR_TEXTDOMAIN ) . '" name="state">';
-		        echo '<option value="*">' . __('All States', SEUR_TEXTDOMAIN ) . '</option>';
-		        foreach( $countryArr as $state => $value ){
+	    if ( $rate == 'B2C Estándar' || $rate == 'SEUR 13:30 Estándar' || $rate == 'SEUR 10 Estándar' || $rate == 'SEUR 10 Frío' || $rate == 'SEUR 13:30 Frío' ) {
 
-		            echo '<option value="' . $state . '">' . $value . '</option>';
-		        }
-		        echo "</select>";
-		    }
+		    if( $countryArr ){
+		    // Display city dropdown based on country name
+			   if( $country !== 'Select' && $country !== 'NULL' ) {
+			        echo '<select title="' . __( 'Select State', SEUR_TEXTDOMAIN ) . '" name="state">';
+			        echo '<option value="*">' . __('All States', SEUR_TEXTDOMAIN ) . '</option>';
+			        foreach( $countryArr as $state => $value ){
+
+			            echo '<option value="' . $state . '">' . $value . '</option>';
+			        }
+			        echo "</select>";
+			   }
+		   }
 	    }
+
+	    if ( $rate == 'SEUR 48H Estándar' ) {
+		    echo '<select title="' . __( 'Select State', SEUR_TEXTDOMAIN ) . '" name="state">';
+			echo '<option value="PM">' . __('Baleares', SEUR_TEXTDOMAIN ) . '</option>';
+			echo '<option value="CE">' . __('Ceuta', SEUR_TEXTDOMAIN ) . '</option>';
+			echo '<option value="ML">' . __('Melilla', SEUR_TEXTDOMAIN ) . '</option>';
+			echo '</select>';
+	    }
+
+	    if ( $rate == 'SEUR 72H Estándar' ) {
+		    echo '<select title="' . __( 'Select State', SEUR_TEXTDOMAIN ) . '" name="state">';
+			echo '<option value="TF">' . __('Las Palmas', SEUR_TEXTDOMAIN ) . '</option>';
+			echo '<option value="GC">' . __('Santa Cruz de Tenerife', SEUR_TEXTDOMAIN ) . '</option>';
+			echo '</select>';
+	    }
+
 	    if ( $country == '*' && ! $countryArr ) {
 		    //campo
 		    echo '<input title="' . __( 'No needed', SEUR_TEXTDOMAIN ) . '" type="text" name="state" class="form-control" placeholder="' . __( 'No needed', SEUR_TEXTDOMAIN ) . '" value="*" readonly>';
@@ -55,6 +84,7 @@ function seur_country_state_process() {
 	    elseif ( $country !== '*' && ! $countryArr ) {
 		    echo '<input title="' . __('SEUR field description', SEUR_TEXTDOMAIN ) . '" type="text" name="state" class="form-control" placeholder="' . __('EX : State', SEUR_TEXTDOMAIN ) . '" required="">';
 	    }
+	    delete_transient( get_current_user_id() . '_seur_rate' );
 	}
 }
 ?>
