036bcf27fd94f29e24b435cf7e22669ecf7393bd
José Conti <j.conti@joseconti.com>
2018-03-09 04:37:49 +0100

    Code is Poetry

--- loader.php
+++ loader.php
@@ -15,7 +15,7 @@ License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
 
-define( 'SEUR_OFFICIAL_VERSION',    '1.0.1'                 );
+define( 'SEUR_OFFICIAL_VERSION',    '1.0.1'                     );
 define( 'SEUR_DB_VERSION',          '1.0.3'                     );
 define( 'SEUR_TABLE_VERSION',       '1.0.0'                     );
 define( 'SEUR_PLUGIN_PATH',         plugin_dir_path( __FILE__ ) );
@@ -30,7 +30,7 @@ define( 'SEUR_PLUGIN_URL',          plugin_dir_url( __FILE__ )  );
 
 require_once( SEUR_PLUGIN_PATH . 'core/loader-core.php'					);
 require_once( SEUR_PLUGIN_PATH . 'core/installer.php'					);
-require_once( SEUR_PLUGIN_PATH . 'include/TGMPA/plugin-install.php'	);
+require_once( SEUR_PLUGIN_PATH . 'include/TGMPA/plugin-install.php'	    );
 
 register_activation_hook( __FILE__, 'seur_create_tables_hook'           );
 register_activation_hook( __FILE__, 'seur_add_data_to_tables_hook'      );
