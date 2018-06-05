52ea68f048d18fc8901b47f3cf1f5e87582e6d26
José Conti <j.conti@joseconti.com>
2018-06-05 23:10:19 +0200

    fixed typo error

--- core/installer.php
+++ core/installer.php
@@ -489,7 +489,7 @@ function seur_create_content_for_download(){
     $content .= '          if ( file_exists( $file ) ) {'                                               . PHP_EOL;
     $content .= ''                                                                                      . PHP_EOL;
     $content .= '                header("Content-Disposition: attachment; filename=" . $name );'        . PHP_EOL;
-    $content .= '                header("Content-type: " . $headerconten );'                            . PHP_EOL;
+    $content .= '                header("Content-type: " . $headercontent );'                           . PHP_EOL;
     $content .= '                header("Expires: 0");'                                                 . PHP_EOL;
     $content .= '                header("Cache-Control: must-revalidate");'                             . PHP_EOL;
     $content .= '                header("Pragma: public");'                                             . PHP_EOL;
